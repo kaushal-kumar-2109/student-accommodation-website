96b66bcdce66b0e92df8fda5499ee829b33348a6
kishor <153196042+kaushal-kumar-2109@users.noreply.github.com>
2026-05-30 14:29:06 +0530

    little update

--- backend/index.php
+++ backend/index.php
@@ -3,6 +3,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Content-Type: application/json");
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
     exit;
